53ef59f9243da7fa250e2a5f66c773621e880462
LudovicArduino <ludovicarduino@gmail.com>
2021-05-25 17:01:19 +0200

    maj

---
+++ assets/js/ajout.js
@@ -0,0 +1,12 @@
+let addQtyToRef = (ref) => {
+    let basket = localStorage.getItem('basket');
+    basket = JSON.parse(basket);
+    basket.forEach((element) => {
+        if(element[0] == ref){
+            element[1]++;
+        }
+    });
+    localStorage.setItem("basket", JSON.stringify(basket));
+};
+
+addQtyToRef();
