e141ddcb2decd2043c381b50c1eef61cf5e8feaa
vincentmancheron <vmancheron@yahoo.fr>
2021-05-25 11:28:18 +0200

    25/05/21 11:28

--- assets/js/cart.js
+++
@@ -1,8 +0,0 @@
-const modalBody = document.querySelector('.modal-body');
-var add = document.querySelectorAll('.add');
-
-add.forEach(element => {
-    element.onclick = () => {
-        modalBody.innerHTML = element.id;
-    }
-});
