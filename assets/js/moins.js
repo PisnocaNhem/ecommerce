0bd0bb43c9d3431ecc3940cfe7fd30c93e23ed21
LudovicArduino <ludovicarduino@gmail.com>
2021-05-25 17:45:04 +0200

    maj

---
+++ assets/js/moins.js
@@ -0,0 +1,12 @@
+let minQtyToRef = (ref) => {
+    let basket = localStorage.getItem('basket');
+    basket = JSON.parse(basket);
+    basket.forEach((element) => {
+        if(element[0] == ref){
+            element[1]--;
+        }
+    });
+    localStorage.setItem("basket", JSON.stringify(basket));
+};
+
+minQtyToRef();
