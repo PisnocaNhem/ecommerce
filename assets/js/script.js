e671252b3a86f9ca11e804ad26241ec673c88cd5
PisnocaNhem <pisnoca_nhem@hotmail.fr>
2021-05-26 15:32:44 +0200

    v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,7 @@ const minQtyToRef = (ref, count) => {
     localStorage.setItem('refLS', JSON.stringify(refArray));
 };
 
-fetch('assets/json/banque.json')
+fetch('https://pisnocanhem.github.io/ecommerce/assets/json/banque.json')
     .then(response => response.json())
     .then(data => {
         display.forEach(element => {
